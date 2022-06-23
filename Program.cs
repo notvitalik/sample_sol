05f51a17b9ff117390e4425c19c9400200636755
!vitalik <36517411+notvitalik@users.noreply.github.com>
2022-06-22 21:53:23 -0400

    add comments - problem statement
    added samples , comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+//write a program that parses a sentence and replaces each word with the following: 
+//first letter, number of distinct characters between first and last character, and last letter.
+//Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer. 
+
+//Examples: 
+//“Smooth” becomes “S3h”
+//“Space separated” becomes “S3e-s5d”
+//“Dash-separated” becomes “D2h-s5d”
+//“Number2separated” becomes “N4r2s5d” 
 using System;
 using System.Text;
 using System.Collections.Generic;
